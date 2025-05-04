4e3afbf09dea773b9046dd393ee2ced00f5dd59c
nastyanebuch12 <145349015+nastyanebuch12@users.noreply.github.com>
2025-05-04 18:31:34 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,56 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+ private:
+  struct Item {
+   T data;
+   Item *next;
+   explicit Item(T data) : data(data), next(nullptr) {}
+  };
+  Item *head, *tail;
+
+ public:
+  TPQueue() : head(nullptr), tail(nullptr) {}
+  ~TPQueue() {
+    while (head) {
+      Item *temp = head;
+      head = head->next;
+      delete temp;
+    }
+  }
+
+  void push(T value) {
+    Item* newItem = new Item(value);
+    if (!head || value.prior > head->data.prior) {
+      newItem->next = head;
+      head = newItem;
+      if (!tail) tail = head;
+      return;
+    }
+    Item* current = head;
+    while (current->next && current->next->data.prior >= value.prior) {
+      current = current->next;
+    }
+    newItem->next = current->next;
+    current->next = newItem;
+    if (!newItem->next) {
+      tail = newItem;
+    }
+  }
+
+  T pop() {
+    if (!head) {
+      throw std::runtime_error("Очередь пуста!");
+    }
+    Item* temp = head;
+    T result = temp->data;
+    head = head->next;
+    if (!head) {
+      tail = nullptr;
+    }
+    delete temp;
+    return result;
+  }
 };
 
 struct SYM {
